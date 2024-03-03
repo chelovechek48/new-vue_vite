de4808fccc97800909eaa2081103701226f5fb27
Maksim Ivanyukhin <chelovechek.maks@mail.ru>
2024-03-03 22:27:38 +0300

    Update

---
+++ vite.config.dev.js
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import { fileURLToPath, URL } from "url";
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    port: "8080",
+  },
+  plugins: [vue()],
+  resolve: {
+    alias: [
+      { find: '@', replacement: fileURLToPath(new URL('./src', import.meta.url)) },
+      { find: '@images', replacement: fileURLToPath(new URL('./src/assets/images', import.meta.url)) },
+      { find: '@icons', replacement: fileURLToPath(new URL('./src/assets/icons', import.meta.url)) },
+      { find: '@fonts', replacement: fileURLToPath(new URL('./src/assets/fonts', import.meta.url)) },
+    ],
+  },
+})
---
+++ vite.config.prod.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import { fileURLToPath, URL } from "url";
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  // base: "/<REPO>"
+  base: '/new-vue>',
+  plugins: [vue()],
+  resolve: {
+    alias: [
+      { find: '@', replacement: fileURLToPath(new URL('./src', import.meta.url)) },
+      { find: '@images', replacement: fileURLToPath(new URL('./src/assets/images', import.meta.url)) },
+      { find: '@icons', replacement: fileURLToPath(new URL('./src/assets/icons', import.meta.url)) },
+      { find: '@fonts', replacement: fileURLToPath(new URL('./src/assets/fonts', import.meta.url)) },
+    ],
+  },
+})
