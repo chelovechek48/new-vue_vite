0d91562208a34b2ded096d955e05c2a0cb1f5a63
Maksim Ivanyukhin <chelovechek.maks@mail.ru>
2024-03-03 22:49:27 +0300

    Testtttt

--- vite.config.dev.js
+++ vite.config.dev.js
@@ -4,8 +4,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: "/<REPO>"
-  base: '/new-vite-vue',
   server: {
     port: "8080",
   },
